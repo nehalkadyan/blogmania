0d92646c4096551540a6e2db240aa55e1aa83924
Nehal Kadyan <89070793+nehalkadyan@users.noreply.github.com>
2024-05-09 14:41:53 +0530

    Update Homepage.jsx

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -23,7 +23,7 @@ const Homepage = () => {
     window.scrollTo(0, 0);
     const fetchPosts = async () => {
       try {
-        let url = `api/posts/allposts`;
+        let url = `https://blogmania-1.onrender.com/api/posts/allposts`;
         if (selectedCategory !== null && selectedCategory !== "All") {
           url += `?category=${selectedCategory}`;
         }
@@ -107,7 +107,7 @@ const Homepage = () => {
               className="bg-white rounded-lg shadow-md overflow-hidden"
             >
               <img
-                src={post?.image}
+                src={`https://blogmania-1.onrender.com/${post?.image}`}
                 alt={post.title}
                 className="w-full h-48 object-cover"
               />
@@ -119,7 +119,7 @@ const Homepage = () => {
                   <div className="flex items-center gap-2">
                     <img
                       className="w-10 h-10 rounded-full object-cover"
-                      src={post?.createdBy?.image}
+                      src={`https://blogmania-1.onrender.com/${post?.createdBy?.image}`}
                       alt="user"
                     />
                     <p className="font-semibold">{post?.createdBy?.email}</p>
