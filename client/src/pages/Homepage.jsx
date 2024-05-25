998722fef2dc90953b08c4106f9f46f617c8438d
Nehal Kadyan <89070793+nehalkadyan@users.noreply.github.com>
2024-05-25 18:55:24 +0530

    Update Homepage.jsx

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -31,7 +31,7 @@ const Homepage = () => {
     window.scrollTo(0, 0);
     const fetchPosts = async () => {
       try {
-        let url = `${process.env.REACT_APP_API_URL}/api/posts/allposts`;
+        let url = `https://blogmania-1.onrender.com/api/posts/allposts`;
         if (selectedCategory !== null && selectedCategory !== "All") {
           url += `?category=${selectedCategory}`;
         }
@@ -115,7 +115,7 @@ const Homepage = () => {
               className="bg-white rounded-lg shadow-md overflow-hidden"
             >
               <img
-                src={"http://localhost:5000/" + post?.image}
+                src={"https://blogmania-1.onrender.com/" + post?.image}
                 alt={post.title}
                 className="w-full h-48 object-cover"
               />
@@ -130,7 +130,7 @@ const Homepage = () => {
                       src={
                         creatorImageContainsHttp(post?.createdBy?.image)
                           ? post?.createdBy?.image
-                          : "http://localhost:5000/" + post?.createdBy?.image
+                          : "https://blogmania-1.onrender.com/" + post?.createdBy?.image
                       }
                       alt="user"
                     />
