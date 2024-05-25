9aadb0dd37c40fb0abcf56edf3f33887374b6412
Nehal Kadyan <89070793+nehalkadyan@users.noreply.github.com>
2024-05-25 22:38:39 +0530

    Update update.route.js

--- api/routes/update.route.js
+++ api/routes/update.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { updateUser } = require("../controllers/update.controller");
 const multer = require("multer");
-const upload = multer({ dest: "../uploads/" });
+const upload = multer({ dest: "uploads/" });
 
 router.put("/users/:id", upload.single("image"), updateUser);
 
