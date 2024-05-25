dddcdb172b8cce1485593c2fd334323c02e53da9
Nehal Kadyan <89070793+nehalkadyan@users.noreply.github.com>
2024-05-25 22:48:39 +0530

    Update EditProfile.jsx

--- client/src/components/profile/EditProfile.jsx
+++ client/src/components/profile/EditProfile.jsx
@@ -142,7 +142,7 @@ const EditProfile = () => {
                   ? imagePreview
                   : containsHttp
                   ? currentUser.image
-                  : currentUser?.image
+                  : `https://blogmania-1.onrender.com/${currentUser?.image}`
               }
               alt="user"
             />
