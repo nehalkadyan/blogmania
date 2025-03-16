fbce5d22e8c115c9d21b5ae023997089f7f1e957
Nehal Kadyan <89070793+nehalkadyan@users.noreply.github.com>
2025-03-17 00:51:32 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -41,7 +41,7 @@ const Signup = () => {
 
     try {
       setLoading(true);
-      const res = await fetch(`http://localhost:5000/api/auth/signup`, {
+      const res = await fetch(`https://blogmania-2-0.onrender.com//api/auth/signup`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
